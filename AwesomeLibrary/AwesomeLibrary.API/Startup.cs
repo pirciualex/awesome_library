b11855fad3e92efd42496e17f1ab6da7af388622
Alex Pirciu <alex.pirciu@zohomail.eu>
2022-07-11 14:55:05 +0300

    Initial setup

---
+++ AwesomeLibrary/AwesomeLibrary.API/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace AwesomeLibrary.API
+{
+    public class Startup
+    {
+        // Dependency injection container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // Application middleware
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(enpoints =>
+            {
+                enpoints.MapControllers();
+            });
+        }
+    }
+}
